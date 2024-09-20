2012c631a1a4ebca149c7e78d203a3cb579d7a55
Lance Hemphill <lance_hemphill1234@yahoo.com>
2024-09-19 20:43:29 -0400

    added navigation buttons, fixed some styles

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,17 @@
 
-
+import { useNavigate } from 'react-router-dom'
 import titleCSS from '../styles/Title.module.css'
 
 const Title = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className={titleCSS.title_container}>
             <p>Welcome to the career of Demography</p>
+
+            <button type='button' onClick={() => navigate("/information")}>Information</button>
+            <button type='button' onClick={() => navigate("/overview")}>overview</button>
         </div>
     )
 }
